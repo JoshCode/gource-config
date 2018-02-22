b969facd4d9a466b08ef7f65638c860ade2d8e2a
JoshCode <joshcodedev@gmail.com>
2018-02-22 01:22:56 +0100

    Add Node script to fetch Gravatars

--- gource/fetch_gravatars.js
+++ gource/fetch_gravatars.js
@@ -1,9 +1,56 @@
-const fs = require('fs');
-const { execSync } = require('child_process');
+const fs     = require('fs'),
+	  crypto = require('crypto'),
+	  util   = require('util'),
+	  http   = require('http'),
+	  Stream = require('stream').Transform;
+const {execSync} = require('child_process');
 
-if(!fs.existsSync('./avatars')) {
-  fs.mkdirSync('./avatars')
+let output_dir = "../.git/avatars";
+let size = 200;
+
+const download = function (url, filename) {
+	return new Promise((resolve, reject) => {
+		http.request(url, (res) => {
+			var data = new Stream();
+
+			res.on('data', function (chunk) {
+				data.push(chunk);
+			});
+
+			res.on('end', function () {
+				if(res.statusCode === 200) {
+					fs.writeFileSync(filename, data.read());
+				}
+				resolve();
+			});
+		}).end();
+	});
+};
+
+if (!fs.existsSync(output_dir)) {
+	fs.mkdirSync(output_dir)
 }
 
-let authors[] = execSync('git log --pretty=format:"%ae|%an').toString().split('\n');
-console.log(authors);
+console.log("Reading git log")
+let authors = execSync('git log --pretty=format:"%ae|%an').toString().split('\n');
+
+console.log("Fetching Gravatars")
+let promises = [];
+for (let i = 0; i < authors.length; i++) {
+	let author = authors[i].split("|");
+	author[0] = author[0].trim().toLowerCase();
+	console.log(`Fetching for '${author[1]}': ${author[0]}`)
+	let hash = crypto.createHash('md5').update(author[0]).digest("hex")
+	let url = `http://www.gravatar.com/avatar/${hash}?d=404&r=g&size=${size}`
+	console.log(`  url: ${url}`)
+	let promise = download(url, `${output_dir}/${author[1]}.png`)
+	promise.then(() => {
+		console.log(`Fetching for '${author[1]}': ${author[0]} complete`)
+	});
+	promises.push(promise);
+}
+
+Promise.all(promises).then(() => {
+	console.log("All fetching complete");
+	process.exit(0);
+})
